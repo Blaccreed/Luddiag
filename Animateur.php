f0dc9ff91b9a9d001f0d7a900432913757f23c8d
Blaccreed <62603834+Blaccreed@users.noreply.github.com>
2022-01-14 22:17:20 +0100

    Ajout Animateur Organisateur

--- Animateur.php
+++ Animateur.php
@@ -4,8 +4,9 @@ Class Animateur extends User
 {
     private string $stand;
 
-    public function __construct($id_user, $nom_user, $prenom_user, $mdp_user, $mail_user, $phone_user, $adresse_user, $cd_postal_user,$stand)
+    public function __construct($id_user=NULL, $nom_user=NULL, $prenom_user=NULL, $mdp_user=NULL, $mail_user=NULL, $phone_user=NULL, $adresse_user=NULL, $cd_postal_user=NULL, $stand=NULL)
     {
+      if(!is_null($id_user))
         parent::__construct($id_user, $nom_user, $prenom_user, $mdp_user, $mail_user, $phone_user, $adresse_user, $cd_postal_user);
         $this->stand = $stand;
     }
@@ -19,6 +20,8 @@ Class Animateur extends User
     {
         return $this->stand;
     }
+
+    public function ValiderNote()
 }
 
 ?>
--- Organisateur.php
+++ Organisateur.php
@@ -2,26 +2,43 @@
 
 Class Organisateur extends User
 {
-    private string $fonction;
+  private string $fonction;
 
-    public function __construct($id_user=NULL, $nom_user=NULL, $prenom_user= NULL, $mdp_user=NULL, $mail_user= NULL, $phone_user = NULL, $adresse_user=NULL, $cd_postal_user= NULL,$fonction= NULL)
+  public function __construct($id_user=NULL, $nom_user=NULL, $prenom_user= NULL, $mdp_user=NULL, $mail_user= NULL, $phone_user = NULL, $adresse_user=NULL, $cd_postal_user= NULL,$fonction= NULL)
+  {
+    if(!is_null($id_user))
     {
-      if(!is_null($id_user))
-      {
-        parent::__construct($id_user, $nom_user, $prenom_user, $mdp_user, $mail_user, $phone_user, $adresse_user, $cd_postal_user);
-        $this->fonction = $fonction;
-      }
+      parent::__construct($id_user, $nom_user, $prenom_user, $mdp_user, $mail_user, $phone_user, $adresse_user, $cd_postal_user);
+      $this->fonction = $fonction;
     }
+  }
 
-    public function GetFonction()
-    {
-       return $this->fonction;
-    }
+  public function GetFonction()
+  {
+    return $this->fonction;
+  }
 
-    public function SetFonction($fonction)
-    {
-       $this->fonction = $fonction;
+  public function SetFonction($fonction)
+  {
+    $this->fonction = $fonction;
+  }
+
+  public static function AjouterExposant($nom_user, $prenom_user, $mdp_user, $mail_user, $phone_user, $adresse_user, $cd_postal_user, $type_exposant)
+  {
+    $requetePreparee = "INSERT INTO voiture VALUES(:tag_immatriculation, :tag_marque,:tag_couleur);";
+
+    $req_prep = Connexion::pdo()->prepare($requetePreparee);
+
+    $arrayName = array("tag_immatriculation" => $immat,
+    "tag_marque" => $marque,
+    "tag_couleur" => $couleur);
+
+    try {
+      $req_prep->execute($arrayName);
+    } catch (PDOException $e) {
+      echo "erreur: ".$e ->getMessage()."</br>";
     }
+  }
 }
 
 ?>
