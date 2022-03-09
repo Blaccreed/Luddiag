3a601416d1b335fa8187f146f5919f4f4ded8831
Clément Legros <71426481+ClementLegros@users.noreply.github.com>
2022-03-09 15:04:15 +0100

    Change Mysql db connection to PostgreSQL

--- Connexion.php
+++ Connexion.php
@@ -2,10 +2,11 @@
 class Connexion
 {
     // attributs de la classe Connexion paramètres de connexion à la base
-    private static $hostname = 'localhost';
-    private static $database = 'luddiag';
-    private static $login = 'root';
-    private static $password = 'root';
+    private static $hostname = 'ec2-79-125-93-182.eu-west-1.compute.amazonaws.com';
+    private static $database = 'd1fed40j8f36go';
+    private static $login = 'hkyuvxishugyfr';
+    private static $password = 'c31891e28d73419204922937201ff7ef899e30ad7dcbe50767b67a10a75565c1';
+    private static $port = 5432;
 
     // attribut de la classe Connexion paramètres d'encodage
     private static $tabUTF8 = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
@@ -20,13 +21,21 @@ class Connexion
     // fonction de connexion
     public static function connect()
     {
-        $h = self::$hostname;
-        $d = self::$database;
-        $l = self::$login;
-        $p = self::$password;
-        $t = self::$tabUTF8;
+        $host = self::$hostname;
+        $db = self::$database;
+        $lgn = self::$login;
+        $pass = self::$password;
+        $tf8 = self::$tabUTF8;
+        $port = self::$port;
         try {
-            self::$pdo = new PDO("mysql:host=$h;dbname=$d", $l, $p, $t);
+            self::$pdo = new PDO("pgsql:" . sprintf(
+                "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+                $host,
+                $port,
+                $lgn,
+                $pass,
+                ltrim($db, "/")
+            ));
             self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo "Erreur de connexion !";
