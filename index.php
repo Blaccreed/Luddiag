4fe6577d9b9727d9065b07ee99848aa4102dc4f8
Clément Legros <71426481+ClementLegros@users.noreply.github.com>
2022-06-14 13:13:49 +0200

    test ?

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 session_start();
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
 if (!isset($_GET["uc"])) {
     $uc = "accueil";
@@ -16,9 +20,12 @@ switch ($uc) {
             break;
         }
         if ($_SESSION['role'] == 'Exposant') {
+
+            ini_set('display_errors', 1);
+            ini_set('display_startup_errors', 1);
+            error_reporting(E_ALL);
             
-            
-        require_once './Config/Connection.php';
+            require_once './Config/Connection.php';
             //On recupere le jeux de l'exposant
             require_once './Model/Jeu.php';
             $list = Jeu::GetJeuExposant($_SESSION['id_user']);
